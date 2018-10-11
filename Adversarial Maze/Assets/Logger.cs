dfa6c62b2f78bc4106626be45a7e32991c8438ce
“Bugs <beckerlethcoe@gmail.com>
2018-10-11 00:42:51 -0500

    Adding timer & logger scripts

---
+++ Adversarial Maze/Assets/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Logs param time to log.txt file in Assets/Resources
+    void LogTime(float time)
+    {
+        string path = "Assets/Resources/log.txt";
+
+        //Write to file
+        System.IO.StreamWriter writer = new System.IO.StreamWriter(path, true);
+        writer.WriteLine(System.DateTime.Now + ":: Time = " + time);
+        writer.Close();
+    }
+
+}
---
+++ Adversarial Maze/Assets/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    private bool isTiming = false;
+    private float time = 0;
+
+	// Use this for initialization
+	void Start () {
+        startTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isTiming)
+        {
+            time += Time.deltaTime;
+        }
+	}
+
+    public float getTime(){
+        return time;
+    }
+
+    public void startTimer(){
+        isTiming = true;
+    }
+
+    public void stopTimer(){
+        isTiming = false;
+    }
+
+    public void resetTimer(){
+        stopTimer();
+        time = 0;
+    }
+}
