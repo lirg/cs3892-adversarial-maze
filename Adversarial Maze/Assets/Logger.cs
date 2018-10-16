a58b684174e9d1d4a718a07d499e950c974e12fa
“Bugs <beckerlethcoe@gmail.com>
2018-10-16 18:04:22 -0500

    Added landmark finding again

--- Adversarial Maze/Assets/Logger.cs
+++ Adversarial Maze/Assets/Logger.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class Logger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    string path;
+    System.IO.StreamWriter writer;
+
+    // Use this for initialization
+    void Start () {
+        //path = "Assets/Resources/log_" + System.DateTime.Now + ".txt";
+        path = "Assets/Resources/myLog.txt";
+        writer = new System.IO.StreamWriter(path, true);
+        writer.WriteLine(System.DateTime.Now + ":: Begin log file");
+        writer.WriteLine("-----------------------------------------");
+        writer.Close();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Debug.Log(System.DateTime.UtcNow + ":: cur time");
 	}
 
     //Logs param time to log.txt file in Assets/Resources
-    void LogTime(float time)
+    public void LogTime(string message, float time)
     {
-        string path = "Assets/Resources/log.txt";
-
         //Write to file
-        System.IO.StreamWriter writer = new System.IO.StreamWriter(path, true);
-        writer.WriteLine(System.DateTime.Now + ":: Time = " + time);
+        writer.WriteLine(System.DateTime.Now + ":: " + message + " at time = " + time);
         writer.Close();
     }
 
