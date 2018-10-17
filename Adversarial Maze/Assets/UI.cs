694326d846c001110036e444b239a922978b4832
Becker Lethcoe <beckerlethcoe@gmail.com>
2018-10-16 22:55:24 -0500

    added preliminary UI stuff and changed landmarks to be logged once per game

---
+++ Adversarial Maze/Assets/UI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    OVRCameraRig rig;
+    Transform camTransform;
+
+    Canvas canvas;
+
+    // Use this for initialization
+    void Start () {
+        rig = FindObjectOfType<OVRCameraRig>();
+        camTransform = rig.centerEyeAnchor;
+
+        canvas.transform.parent = rig.transform;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
