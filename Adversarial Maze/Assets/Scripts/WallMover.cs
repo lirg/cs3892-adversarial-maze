93b2ab3e5ed9a35a6933eb9ac9831234c1872980
Roger Li <roger.g.li@vanderbilt.edu>
2018-12-08 23:10:52 -0600

    Finish wall movements for all pickups

--- Adversarial Maze/Assets/Scripts/WallMover.cs
+++ Adversarial Maze/Assets/Scripts/WallMover.cs
@@ -6,6 +6,10 @@ public class WallMover : MonoBehaviour {
 
     GameObject wall1_1;
     GameObject wall1_2;
+    GameObject wall2_1;
+    GameObject wall3_1;
+    GameObject wall4_1;
+    GameObject wall5_1;
 
     GameObject curMovingWall;
 
@@ -18,6 +22,10 @@ public class WallMover : MonoBehaviour {
     void Start () {
         wall1_1 = GameObject.Find("movablewall1_1");
         wall1_2 = GameObject.Find("movablewall1_2");
+        wall2_1 = GameObject.Find("movablewall2_1");
+        wall3_1 = GameObject.Find("movablewall3_1");;
+        wall4_1 = GameObject.Find("movablewall4_1");;
+        wall5_1 = GameObject.Find("movablewall5_1");;
         startPosition = target = wall1_1.transform.position;
         curMovingWall = wall1_1;
         Debug.Log("Started wallmover");
@@ -47,10 +55,31 @@ public class WallMover : MonoBehaviour {
         curMovingWall = wall;
     }
 
+    private void TranslateZ(GameObject wall, float time, int distance) {
+        t = 0;
+        startPosition = wall.transform.position;
+        timeToReachTarget = time;
+        target = wall.transform.position;
+        target.z += distance;
+        curMovingWall = wall;
+    }
+
     public void moveWalls(string name){
         switch(name){
             case "Pick Up 1":
                 StartCoroutine(move1());
+                break;
+            case "Pick Up 2":
+                StartCoroutine(move2());
+                break;
+            case "Pick Up 3":
+                StartCoroutine(move3());
+                break;
+            case "Pick Up 4":
+                StartCoroutine(move4());
+                break;
+            case "Pick Up 5":
+                StartCoroutine(move5());
                 break;
         }
     }
@@ -60,5 +89,29 @@ public class WallMover : MonoBehaviour {
         TranslateY(wall1_1, 4, -25);
         yield return new WaitForSeconds(4);
         TranslateX(wall1_2, 3, 10);
+    }
+
+    IEnumerator move2() {
+        Debug.Log("Moving walls for pickup 2");
+        TranslateY(wall2_1, 4, -25);
+        yield return new WaitForSeconds(4);
+    }
+
+    IEnumerator move3() {
+        Debug.Log("Moving walls for pickup 3");
+        TranslateZ(wall3_1, 4, 8);
+        yield return new WaitForSeconds(4);
+    }
+
+    IEnumerator move4() {
+        Debug.Log("Moving walls for pickup 4");
+        TranslateX(wall4_1, 4, -10);
+        yield return new WaitForSeconds(4);
+    }
+
+    IEnumerator move5() {
+        Debug.Log("Moving walls for pickup 5");
+        TranslateY(wall5_1, 4, -25);
+        yield return new WaitForSeconds(4);
     }
 }
