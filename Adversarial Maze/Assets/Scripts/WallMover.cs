b68f99db1b04327f5d35297df23a4d4e01e183b7
Roger Li <roger.g.li@vanderbilt.edu>
2018-12-09 00:34:01 -0600

    Add LW scene and insert moving landmarks

--- Adversarial Maze/Assets/Scripts/WallMover.cs
+++ Adversarial Maze/Assets/Scripts/WallMover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WallMover : MonoBehaviour {
 
@@ -11,23 +12,37 @@ public class WallMover : MonoBehaviour {
     GameObject wall4_1;
     GameObject wall5_1;
 
+    GameObject fountain;
+    GameObject horse;
+    GameObject lion;
+    GameObject lamp;
+
     GameObject curMovingWall;
 
     float t;
     Vector3 startPosition;
     Vector3 target;
     float timeToReachTarget;
+    Scene scene;
+    string static_scene = "Static Condition";
+    string dynamic_walls = "Dynamic Walls";
+    string both_dynamic = "Dynamic LW";
 
     // Use this for initialization
     void Start () {
         wall1_1 = GameObject.Find("movablewall1_1");
         wall1_2 = GameObject.Find("movablewall1_2");
         wall2_1 = GameObject.Find("movablewall2_1");
-        wall3_1 = GameObject.Find("movablewall3_1");;
-        wall4_1 = GameObject.Find("movablewall4_1");;
-        wall5_1 = GameObject.Find("movablewall5_1");;
+        wall3_1 = GameObject.Find("movablewall3_1");
+        wall4_1 = GameObject.Find("movablewall4_1");
+        wall5_1 = GameObject.Find("movablewall5_1");
+        fountain = GameObject.Find("Fountain_LW");
+        lion = GameObject.Find("Lion_LW");
+        lamp = GameObject.Find("CarvedLampPost_LW");
+        horse = GameObject.Find("Horse_LW");
         startPosition = target = wall1_1.transform.position;
         curMovingWall = wall1_1;
+        scene = SceneManager.GetActiveScene();
         Debug.Log("Started wallmover");
 	}
 	
@@ -65,22 +80,25 @@ public class WallMover : MonoBehaviour {
     }
 
     public void moveWalls(string name){
-        switch(name){
-            case "Pick Up 1":
-                StartCoroutine(move1());
-                break;
-            case "Pick Up 2":
-                StartCoroutine(move2());
-                break;
-            case "Pick Up 3":
-                StartCoroutine(move3());
-                break;
-            case "Pick Up 4":
-                StartCoroutine(move4());
-                break;
-            case "Pick Up 5":
-                StartCoroutine(move5());
-                break;
+        scene = SceneManager.GetActiveScene();
+        if (scene.name != static_scene) {
+            switch(name){
+                case "Pick Up 1":
+                    StartCoroutine(move1());
+                    break;
+                case "Pick Up 2":
+                    StartCoroutine(move2());
+                    break;
+                case "Pick Up 3":
+                    StartCoroutine(move3());
+                    break;
+                case "Pick Up 4":
+                    StartCoroutine(move4());
+                    break;
+                case "Pick Up 5":
+                    StartCoroutine(move5());
+                    break;
+            }
         }
     }
 
@@ -89,10 +107,18 @@ public class WallMover : MonoBehaviour {
         TranslateY(wall1_1, 4, -25);
         yield return new WaitForSeconds(4);
         TranslateX(wall1_2, 3, 10);
+        yield return new WaitForSeconds(3);
+        if (scene.name == both_dynamic) {
+            TranslateZ(fountain, 2, -25);
+        }        
     }
 
     IEnumerator move2() {
         Debug.Log("Moving walls for pickup 2");
+        if (scene.name == both_dynamic) {
+            TranslateZ(lion, 1, -25);
+            yield return new WaitForSeconds(1);
+        }   
         TranslateY(wall2_1, 4, -25);
         yield return new WaitForSeconds(4);
     }
@@ -101,17 +127,26 @@ public class WallMover : MonoBehaviour {
         Debug.Log("Moving walls for pickup 3");
         TranslateZ(wall3_1, 4, 8);
         yield return new WaitForSeconds(4);
+        if (scene.name == both_dynamic) {
+            TranslateX(lamp, 1, -55);
+        }   
     }
 
     IEnumerator move4() {
         Debug.Log("Moving walls for pickup 4");
         TranslateX(wall4_1, 4, -10);
         yield return new WaitForSeconds(4);
+        if (scene.name == both_dynamic) {
+            TranslateX(horse, 1, -65);
+            yield return new WaitForSeconds(1);
+            TranslateZ(horse, 1, -20);
+            yield return new WaitForSeconds(1);
+        }   
     }
 
     IEnumerator move5() {
         Debug.Log("Moving walls for pickup 5");
         TranslateY(wall5_1, 4, -25);
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(4); 
     }
 }
